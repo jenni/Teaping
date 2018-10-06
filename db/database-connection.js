e64d22c1157269646b30d7e0fdb0410ab9e63999
Jen <jen.lijo@zalando.de>
2018-10-06 17:10:39 +0200

    add mongo string

--- db/database-connection.js
+++ db/database-connection.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/teaping');
+mongoose.connect('mongodb://heroku_s0fzwz9x:ntl9h8gmr5c50fnhj2caqhlntv@ds129422.mlab.com:29422/heroku_s0fzwz9x' || 'mongodb://localhost/teaping');
