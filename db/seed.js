7ce6571bfeed21be5cd1e37993d5da02c7c51e03
masaun <masanoriuno@gmail.com>
2018-10-06 14:20:57 +0200

    Add wallet

--- db/seed.js
+++ db/seed.js
@@ -10,11 +10,18 @@ const worker4 = new Worker({ name: 'Gosia', hash: '345345435435435' });
 const worker5 = new Worker({ name: 'Emsoft', hash: '3253544534646546' });
 const worker6 = new Worker({ name: 'Melanie', hash: '3253544534646546' });
 
+const Wallet = require('../models/wallet-model');
+
+const wallet1 = new Wallet({ name: 'main-wallet', hash: '23122342ddaafba', quantity: 0 });
+
+
+
 
 const starterSeed = async () => {
     const workers = [worker1, worker2, worker3, worker4, worker5, worker6];
 
     for (const worker of workers) await Worker.create(worker);
+    await Wallet.create(wallet1);
 }
 
 mongoose.connection.on('open', async () => {
